d5d10b49751eafae9f9b526618243688e90891d4
Ruchen Liu <rliu@Ruchens-MacBook-Pro.local>
2018-01-29 20:48:40 -0800

    pull

--- src/mlx.c
+++ src/mlx.c
@@ -30,19 +30,19 @@ int		reg_key_events(int keycode, t_env *env)
 		mlx_destroy_window(env->mlx.mlx, env->mlx.win);
 		exit(0);
 	}
-	else if (keycode == UP)
-	else if (keycode == DOWN)
-	else if (keycode == LEFT)
-	else if (keycode == RIGHT)
-	else if (keycode == ZOOMIN)
-	else if (keycode == ZOOMOUT)
+	// else if (keycode == UP)
+	// else if (keycode == DOWN)
+	// else if (keycode == LEFT)
+	// else if (keycode == RIGHT)
+	// else if (keycode == ZOOMIN)
+	// else if (keycode == ZOOMOUT)
 	return (0);
 }
 
-int 	cool_key_events(int keycode, t_env *env)
-{
-	//color, isometric, 
-}
+// int 	cool_key_events(int keycode, t_env *env)
+// {
+// 	//color, isometric, 
+// }
 
 void		mlx(t_env *base)
 {
@@ -52,8 +52,8 @@ void		mlx(t_env *base)
 	 */	
 	base->mlx.mlx = mlx_init();
 	base->mlx.win = mlx_new_window(base->mlx.mlx, WIDTH, HEIGHT, "FDF");
-	mlx_key_hook(base->mlx.win, cool_key_events, base->mlx.mlx);
-	create_fdf(base);
+	mlx_key_hook(base->mlx.win, reg_key_events, base->mlx.mlx);
+	//create_fdf(base);
 	/*	mlx_hook(base->mlx.win, 2, 3, cool_key_events, base);
 	 *	mlx_loop_hook(base->mlx.mlx, create_fdf, base);
 	 * */
